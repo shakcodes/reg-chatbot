c819ee0bbf8f23ba6ecaed44b29b46434d8a9073
Mohd Shakib <117885516+shakcodes@users.noreply.github.com>
2026-03-25 15:07:38 +0530

    Remove from error logging

--- backend/utils/errorHandler.js
+++ backend/utils/errorHandler.js
@@ -1,4 +1,4 @@
 export const errorHandler = (err, req, res, next) => {
-  console.error("❌ Error:", err.message);
+  console.error("Error:", err.message);
   res.status(500).json({ error: err.message || "Server error" });
 };
