981220a55728ddb19a42075f676b89ff69006b90
Mohd Shakib <117885516+shakcodes@users.noreply.github.com>
2026-03-25 15:08:16 +0530

    Remove emoji from console log message

--- backend/testEmbeddings.js
+++ backend/testEmbeddings.js
@@ -2,7 +2,7 @@ import { getEmbeddings } from "./services/embeddings.js";
 
 const run = async () => {
   const vector = await getEmbeddings("Hello world!");
-  console.log("🔹 Embedding vector length:", vector.length);
+  console.log("Embedding vector length:", vector.length);
 };
 
 run();
